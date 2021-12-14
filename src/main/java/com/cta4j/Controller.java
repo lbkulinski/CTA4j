5307c49669e13918f4a3bf1e97495e22717b1529
Logan Kulinski <lbkulinski@users.noreply.github.com>
2021-12-14 13:37:19 -0600

    Controller updates

--- src/main/java/com/cta4j/Controller.java
+++ src/main/java/com/cta4j/Controller.java
@@ -26,6 +26,7 @@ package com.cta4j;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Set;
 import com.cta4j.model.Route;
@@ -54,7 +55,7 @@ public final class Controller {
      *
      * @return a JSON response containing information about routes
      */
-    @GetMapping("get-routes")
+    @GetMapping(value = "get-routes", produces = MediaType.APPLICATION_JSON_VALUE)
     public String getRoutes(@RequestParam(name = "route", required = false) String[] routeNames) {
         if (routeNames == null) {
             routeNames = new String[0];
