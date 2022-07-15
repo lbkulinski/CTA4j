11edd9fe0cdfaeca339021de77b3557864c0b5b2
Logan Kulinski <lbkulinski@users.noreply.github.com>
2022-07-15 09:35:56 -0500

    Doc comment updates

--- src/main/java/com/cta4j/bus/utils/BusUtils.java
+++ src/main/java/com/cta4j/bus/utils/BusUtils.java
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022 Logan Kulinski
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.cta4j.bus.utils;
 
 import com.cta4j.bus.model.adapters.BusTypeAdapter;
@@ -21,12 +45,21 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
 
+/**
+ * A set of utilities used by the CTA4j bus tracker.
+ *
+ * @author Logan Kulinski, rashes_lineage02@icloud.com
+ * @version July 15, 2022
+ */
 public final class BusUtils {
     /**
      * The {@link Logger} of the {@link BusUtils} class.
      */
     private static final Logger LOGGER;
 
+    /**
+     * The bus API key of the {@link BusUtils class}.
+     */
     private static final String BUS_API_KEY;
 
     static {
